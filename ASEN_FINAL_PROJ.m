bf2d6c964b2d7dbbe6391fa13853ea0582a691f1
stevecool2020 <70174621+stevecool2020@users.noreply.github.com>
2021-12-01 17:54:27 -0700

    Update ASEN_FINAL_PROJ.m
    System is not Hurowitz. Some eig-vals lie in the right side of s plane

--- ASEN_FINAL_PROJ.m
+++ ASEN_FINAL_PROJ.m
@@ -1,3 +1,6 @@
+clear all
+close all
+
 %%%%%% A,B,C,D Matrices %%%%%%
 n = sqrt(398600 / 6778^3);
 A = [0 0 0 1 0 0;
@@ -23,6 +26,13 @@ D = [0 0 0;
      0 0 0];
 
 sys = ss(A,B,C,D);
+
+eigVal = eig(A);
+[eig_Vec, eig_ValDiag] = eig(A);
+figure
+plot(eigVal, 'o');
+xlabel('Real Part')
+ylabel('Imaginary Part')
 
 %%% STUB %%%
 
